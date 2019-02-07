50b1bea536a8a6bdafbda18e167b7b7558f6ac88
NycollSM <nycollsotomorales21@gmail.com>
2019-02-06 18:56:55 -0600

    adding value of elements at dom

--- javascript/script.js
+++ javascript/script.js
@@ -1,31 +1,35 @@
 (function(){
-  const table  = document.getElementsByTagName('table');
-  saveBtn = document.getElementById('saveBtn');
-  const form = {
-    nameTxt = document.getElementById('name'),
-    amountTxt = document.getElementById('amout'),
-    dateTxt= document.getElementById('date'),
-    typeTxt = document.getElementById('type')
-  }
-  function showValues(){
-    for( const i of form ){
-      const tableRow = document.createElement('tr');
-      const tableCellName = document.createElement('td');
-      const tableCellType = document.createElement('td');
-      const tableCellAmout = document.createElement('td');
-      const tableCellDate = document.createElement('td');
-      //DOM 
-      tableCellName.innerHTML = i.nameTxt.value;
+  const table  = document.getElementById('table');
+  const  saveBtn = document.getElementById('saveBtn');
+  const  nameTxt = document.getElementById('name');
+  const  amountTxt = document.getElementById('amount');
+  const  dateTxt= document.getElementById('date');
+  const  typeTxt = document.getElementById('type');
 
-      tableCellName.appendChild(tableRow);
-      tableRow.appendChild(table);
-    }    
-      
-    
-    // nameTxt.value = '';
-    // amountTxt.value = '';
-    // dateTxt.value = '';
-    // typeTxt.value = '';
+  function showValues(){
+    const tableRow = document.createElement('tr');
+    const tableCellName = document.createElement('td');
+    const tableCellType = document.createElement('td');
+    const tableCellAmout = document.createElement('td');
+    const tableCellDate = document.createElement('td');
+
+      // DOM 
+      tableRow.appendChild(tableCellName); 
+      tableRow.appendChild(tableCellType);
+      tableRow.appendChild(tableCellAmout);
+      tableRow.appendChild(tableCellDate);
+      table.appendChild(tableRow);
+
+      tableCellName.innerText = nameTxt.value;
+      tableCellType.innerText = typeTxt.value;
+      tableCellAmout.innerText = amountTxt.value;
+      tableCellDate.innerText = dateTxt.value; 
+
+      // clear textarea 
+      nameTxt.value = '';
+      amountTxt.value = '';
+      dateTxt.value = '';
+      typeTxt.value = '';
   }
 
 
