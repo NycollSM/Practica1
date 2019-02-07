c437f420f8428a31805d64b4ac809c2f04ca30f3
NycollSM <nycollsotomorales21@gmail.com>
2019-02-06 19:24:32 -0600

    object of elements

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,18 @@
 (function(){
   const table  = document.getElementById('table');
   const  saveBtn = document.getElementById('saveBtn');
-  const  nameTxt = document.getElementById('name');
-  const  amountTxt = document.getElementById('amount');
-  const  dateTxt= document.getElementById('date');
-  const  typeTxt = document.getElementById('type');
+  const elements = {
+      nameTxt: document.getElementById('name'),
+      amountTxt: document.getElementById('amount'),
+      dateTxt: document.getElementById('date'),
+      typeTxt: document.getElementById('type'),
+  }
+  
 
   function showValues(){
     const tableRow = document.createElement('tr');
+    for (const i in elements){
+    
     const tableCellName = document.createElement('td');
     const tableCellType = document.createElement('td');
     const tableCellAmout = document.createElement('td');
@@ -20,27 +25,17 @@
       tableRow.appendChild(tableCellDate);
       table.appendChild(tableRow);
 
-      tableCellName.innerText = nameTxt.value;
-      tableCellType.innerText = typeTxt.value;
-      tableCellAmout.innerText = amountTxt.value;
-      tableCellDate.innerText = dateTxt.value; 
+      tableCellName.innerText = i.nameTxt;
+      tableCellType.innerText = i.typeTxt;
+      tableCellAmout.innerText = i.amountTxt;
+      tableCellDate.innerText = i.dateTxt; 
 
-      // clear textarea 
-      nameTxt.value = '';
-      amountTxt.value = '';
-      dateTxt.value = '';
-      typeTxt.value = '';
+      
+    }
+
+
   }
 
-
 saveBtn.addEventListener('click' , showValues);
-
-
-
-
-
-
-
-
 
 })();
