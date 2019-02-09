aaf5d5d4fba8cca4192b48b5fff8d4fd7533c9f2
NycollSM <Nycollsotomorales21@gmail.com>
2019-02-08 19:25:23 -0600

    arrays of name and amount

--- javascript/script.js
+++ javascript/script.js
@@ -1,41 +1,44 @@
 (function(){
-  const table  = document.getElementById('table');
-  const saveBtn = document.getElementById('saveBtn');
-  const nameTxt = document.getElementById('name');
-  const dateTxt = document.getElementById('date');
-  const typeTxt = document.getElementById('type');
-  const amountTxt = document.getElementById('amount');
-  
-  const elements = {
-    name : nameTxt.value,
-    date : dateTxt.value,
-    amoun: amountTxt.value,
-    type : typeTxt.value
-  }
-  
-  function showValues(){  
-    
-    console.log(elements);
-    const tableRow = document.createElement('tr');
-    for (const i in elements){
-    const tableCellItem = document.createElement('td');
+  const saveBTn = document.getElementById('saveBtn');
+  const name = document.getElementById('name');
+  const amount = document.getElementById('amount');
+  const date = document.getElementById('date');
+  const type = document.getElementById('type');
+  const table = document.getElementById('tableBody');
+  const list = document.getElementById('list');
+  const bills = [];
+  const namesArray = [];
+  // add values at dom 
+  function showValues() {
+      const tableRow = document.createElement('tr');
+      const tdName = document.createElement('td');
+      const tdAmount = document.createElement('td');
+      const tdDate = document.createElement('td');
+      const tdType = document.createElement('td');
 
-      // DOM 
+      tdName.innerText = name.value;
+      tdAmount.innerText = amount.value;
+      tdDate.innerText = date.value;
+      tdType.innerText = type.value;
+
+      tableRow.appendChild(tdName);
+      tableRow.appendChild(tdAmount);
+      tableRow.appendChild(tdDate);
+      tableRow.appendChild(tdType);
+      table.appendChild(tableRow); 
+
+      namesArray.push(name.value);
       
-      tableRow.appendChild(tableCellItem); 
-      table.appendChild(tableRow);
-
-      tableCellItem.innerText = i.nameTxt;
-      tableCellItem.innerText = i.typeTxt;
-      tableCellItem.innerText = i.amountTxt;
-      tableCellItem.innerText = i.dateTxt; 
-
-      
-    }
-
-
+  
   }
-
-saveBtn.addEventListener('click' , showValues);
-
+  function showBills(){
+    const bill = document.createElement('li');
+    bill.innerText = 'Bill '+ type.value + ' '+ amount.value;
+    list.appendChild(bill);
+    bills.push(amount.value);
+  }
+  console.log(namesArray);
+  console.log(bills);
+  saveBTn.addEventListener('click' , showValues);
+  saveBTn.addEventListener('click', showBills);
 })();
